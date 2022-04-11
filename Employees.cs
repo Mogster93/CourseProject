02d4c02fa270c4c0542601ca2f83f2d4653228c1
Mogster93 <102771220+Mogster93@users.noreply.github.com>
2022-04-10 20:45:46 -0500

    Update Employees.cs

--- Employees.cs
+++ Employees.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +16,63 @@ namespace GBMFDB
         {
             InitializeComponent();
         }
+        class EmployeeDetails {  
+     
+            employees.Add(new Employee)
+            {
+                EmployeeId = 1,
+                EmployeeName = "John Smith"
+
+            };
+
+            employees.Add(new Employee)
+            {
+                EmployeeId = 2,
+                EmployeeName = "Harry Jones";
+
+            };
+
+            employees.Add(new Employee)
+            {
+                EmployeeId = 3,
+                EmployeeName = "William Davis",
+            };
+
+            employees.Add(new Employee)
+            {
+                EmployeeId = 4,
+                EmployeeName = "James Anderson",
+            };
+
+            employees.Add(new Employee)
+            {
+                EmployeeId = 5,
+                EmployeeName = "Gina Rogers",
+            };
+
+            employees.Add(new Employee)
+            {
+                EmployeeId = 6,
+                EmployeeName = "Jack Daniels",
+            };
+
+            employees.Add(new Employee)
+            {
+                EmployeeId = 7,
+                EmployeeName = "Mary Hendricks",
+            };
+
+            employees.Add(new Employee)
+            {
+                EmployeeId = 8,
+                EmployeeName = "Melinda Peters",
+            };
+
+            employees.Add(new Employee)
+            {
+                EmployeeId = 9,
+                EmployeeName = "Oscar Hernandez",
+            };
+        }  
     }
 }
